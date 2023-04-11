531a7c6a837f6ec084f38a6711d45c3ed7a6bbaa
KoheiHamasaki <124167153+kouheihamasaki@users.noreply.github.com>
2023-04-11 03:28:28 +0000

    add-post-starculm

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,11 @@ import '@fortawesome/fontawesome-free/js/all'
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
+import Raty from "raty.js"
+window.raty = function(elem,opt) {
+  let raty =  new Raty(elem,opt)
+  raty.init();
+  return raty;
+}
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_05_031239) do
+ActiveRecord::Schema.define(version: 2023_04_11_032727) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -109,6 +109,7 @@ ActiveRecord::Schema.define(version: 2023_04_05_031239) do
     t.string "shop_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "star"
   end
 
   create_table "tags", force: :cascade do |t|
