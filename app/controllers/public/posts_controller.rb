b9be85dad43f81377866f7db4c7bd9eaa8f13fa6
KoheiHamasaki <124167153+kouheihamasaki@users.noreply.github.com>
2023-04-16 07:59:32 +0000

    fix-post-index

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -4,7 +4,7 @@ class Public::PostsController < ApplicationController
     @post_all = Post.all
     @posts = Post.page(params[:page]).per(10)
     @user = current_user
-    
+
     if params[:tag_ids]
       @posts = []
       params[:tag_ids].each do |key, value|
@@ -14,14 +14,14 @@ class Public::PostsController < ApplicationController
         end
       end
     end
-    
+
     if params[:tag]
       Tag.create(name: params[:tag])
     end
-    
+
   end
-  
-  
+
+
   def show
     @post = Post.find(params[:id])
     @user = current_user
@@ -29,8 +29,12 @@ class Public::PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+    unless @post.user_id == current_user.id
+     redirect_to posts_path
+    end
   end
-  
+
   def new
     @post = Post.new
   end
@@ -38,14 +42,21 @@ class Public::PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
-    if @post.save
+    if @post.save!
     redirect_to posts_path
     else
     render :new
     end
   end
-  
+
   def update
+    @post = Post.find(params[:id])
+    @post.user_id = current_user.id
+    if @post.update(post_params)
+     redirect_to post_path(@post.id)
+    else
+     render :edit
+    end
   end
 
   def destroy
@@ -56,13 +67,13 @@ class Public::PostsController < ApplicationController
     render :index
     end
   end
-  
-  
-  
+
+
+
   private
 
   def post_params
     params.require(:post).permit(:shop_name, :image, :title, :body, :star, :adress, :latitude, :longitude, tag_ids: [])
   end
-  
+
 end
