47d497f5b44e3359568413267c25299d920f5c32
KoheiHamasaki <124167153+kouheihamasaki@users.noreply.github.com>
2023-04-19 08:53:21 +0000

    toppage

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -3,6 +3,11 @@ class Public::HomesController < ApplicationController
 
   def top
     @user = current_user
+    @posts_new = Post.order('id DESC').limit(3)
+    @posts_fav = Post.find(Favorite.group(:post_id).order('count(post_id) desc').pluck(:post_id))
+    @posts_fav_tops =@posts_fav.first(3)
+    @user_post_tops = User.where(id: Post.group(:user_id).order('count(user_id) desc').pluck(:user_id))
+    @user_ranks = @user_post_tops.first(3)
   end
 
   def about
