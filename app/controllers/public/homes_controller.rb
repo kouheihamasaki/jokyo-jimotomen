ed3124f197bd4a7aeb6d41b29709af2f6dc0976b
KoheiHamasaki <124167153+kouheihamasaki@users.noreply.github.com>
2023-04-21 14:39:23 +0000

    add-top-finish

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -6,8 +6,7 @@ class Public::HomesController < ApplicationController
     @posts_new = Post.order('id DESC').limit(3)
     @posts_fav = Post.find(Favorite.group(:post_id).order('count(post_id) desc').pluck(:post_id))
     @posts_fav_tops =@posts_fav.first(3)
-    @user_post_tops = User.where(id: Post.group(:user_id).order('count(user_id) desc').pluck(:user_id))
-    @user_ranks = @user_post_tops.first(3)
+    @sample_post = Post.all.shuffle.first
     # サイドバー部分
     post_fav_bests = current_user.post.sort { |a, b| b.favorite.count <=> a.favorite.count }
     @post_fav_best = post_fav_bests.first(1)
