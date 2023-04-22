33507438c7a2a4d62b9e9a8f979e31c572ee0b02
KoheiHamasaki <124167153+kouheihamasaki@users.noreply.github.com>
2023-04-22 03:58:09 +0000

    admintop-footer

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,6 +1,10 @@
 class Admin::HomesController < ApplicationController
 
  def top
-   @users = User.page(params[:page]).per(10)
+  @posts_new = Post.order('id DESC').limit(3)
+  @posts_fav = Post.find(Favorite.group(:post_id).order('count(post_id) desc').pluck(:post_id))
+  @posts_fav_tops =@posts_fav.first(3)
+  @sample_post = Post.all.shuffle.first
  end
+
 end
--- app/controllers/public/communities_controller.rb
+++ app/controllers/public/communities_controller.rb
@@ -5,12 +5,20 @@ class Public::CommunitiesController < ApplicationController
     @user = current_user
     @community_all = Community.all
     @communities = Community.page(params[:page]).per(10)
+
+    # サイドバー部分
+    post_fav_bests = current_user.post.sort { |a, b| b.favorite.count <=> a.favorite.count }
+    @post_fav_best = post_fav_bests.first(1)
   end
 
   def show
     @user = current_user
     @community = Community.find(params[:id])
     @community_comment = CommunityComment.new
+
+    # サイドバー部分
+    post_fav_bests = current_user.post.sort { |a, b| b.favorite.count <=> a.favorite.count }
+    @post_fav_best = post_fav_bests.first(1)
   end
 
 
