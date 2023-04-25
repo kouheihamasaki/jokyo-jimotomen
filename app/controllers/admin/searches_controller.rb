a4e47833a8a997fa516f77dfc0a55e7b4963049f
KoheiHamasaki <124167153+kouheihamasaki@users.noreply.github.com>
2023-04-25 07:44:29 +0000

    add-search-admin

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,2 +1,18 @@
 class Admin::SearchesController < ApplicationController
+
+  def search
+    @range = params[:range]
+
+    if @range == "投稿記事"
+      @posts = Post.looks(params[:word])
+      @result_word = params[:word]
+      render "admin/searches/search_result"
+    else
+      @communities = Community.looks(params[:word])
+      @result_word = params[:word]
+      render "admin/searches/search_result"
+    end
+
+  end
+
 end
