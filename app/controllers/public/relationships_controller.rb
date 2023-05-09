dab1462b7c8c08b2d7de97b392357d27a005edf1
KoheiHamasaki <124167153+kouheihamasaki@users.noreply.github.com>
2023-05-09 03:59:01 +0000

    fix-follow-feature

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -5,24 +5,24 @@ class Public::RelationshipsController < ApplicationController
     current_user.follow(params[:user_id])
     redirect_to request.referer
   end
-  
+
   # フォロー外すとき
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer  
+    redirect_to request.referer
   end
-  
+
   # フォロー一覧
   def followings
-  user = User.find(params[:user_id])
-  @users = user.followings
+    user = current_user
+    @users = user.followings
   end
-  
+
   # フォロワー一覧
   def followers
-  user = User.find(params[:user_id])
-  @users = user.followers
+    user = User.find(params[:user_id])
+    @users = user.followers
   end
-  
+
 
 end
